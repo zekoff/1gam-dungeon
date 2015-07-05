f1e44721f6f6169b6223f6cfb5eb22fd30cc5de1
Michael Zekoff <zekoff@gmail.com>
2015-07-05 22:56:58 +0000

    Switch to array-based map definition.

--- src/state/main.js
+++ src/state/main.js
@@ -8,6 +8,27 @@ var player;
 var layer;
 var moveTarget;
 
+var createDungeon = function() {
+    var mapArray = [];
+    var i;
+    var j;
+    for (i = 0; i < WORLD_WIDTH; i++) mapArray.push([]);
+    for (i = 0; i < WORLD_WIDTH; i++)
+        for (j = 0; j < WORLD_HEIGHT; j++) mapArray[i][j] = 0;
+    for (i = 0; i < WORLD_WIDTH; i++) {
+        mapArray[i][0] = 1;
+        mapArray[i][WORLD_HEIGHT - 1] = 1;
+    }
+    for (i = 0; i < WORLD_HEIGHT; i++) {
+        mapArray[0][i] = 1;
+        mapArray[WORLD_WIDTH - 1][i] = 1;
+    }
+    mapArray[4][8] = 1;
+    mapArray[10][2] = 1;
+    mapArray[14][12] = 1;
+    return mapArray;
+};
+
 state.create = function() {
     player = game.add.sprite(50, 50, 'pix');
     player.height = 20;
@@ -18,24 +39,19 @@ state.create = function() {
     game.camera.roundPx = false;
     game.physics.arcade.enable(player);
     var map = game.add.tilemap();
-    map.addTilesetImage('test_tileset');
+    map.addTilesetImage('test_tileset', 'test_tileset', 32, 32, 32, 32, 0);
     layer = map.create('walls', WORLD_WIDTH, WORLD_HEIGHT, 32, 32);
     layer.resizeWorld();
-    map.putTile(1, 4, 8);
-    map.putTile(1, 10, 2);
-    var i;
-    for (i = 0; i < WORLD_WIDTH; i++) {
-        map.putTile(1, i, 0);
-        map.putTile(1, i, WORLD_HEIGHT - 1);
-    }
-    for (i = 0; i < WORLD_HEIGHT; i++) {
-        map.putTile(1, 0, i);
-        map.putTile(1, WORLD_WIDTH - 1, i);
-    }
+    var mapArray = createDungeon();
+    var i, j;
+    for (i = 0; i < WORLD_WIDTH; i++)
+        for (j = 0; j < WORLD_HEIGHT; j++)
+            map.putTile(mapArray[i][j], i, j);
     map.setCollision(1);
     game.physics.arcade.setBoundsToWorld();
     player.body.collideWorldBounds = true;
     game.input.onUp.add(function() {
+        if (moveTarget) moveTarget.destroy();
         moveTarget = game.add.sprite(game.input.activePointer.worldX,
             game.input.activePointer.worldY, 'pix');
         moveTarget.width = 5;
@@ -46,16 +62,14 @@ state.create = function() {
     });
 };
 
+var reachedDestination = function() {
+    player.body.velocity.set(0);
+    if (moveTarget) moveTarget.destroy();
+};
+
 state.update = function() {
-    game.physics.arcade.collide(player, layer, function() {
-        player.body.velocity.set(0);
-        if (moveTarget) moveTarget.destroy();
-    });
-    if (moveTarget)
-        game.physics.arcade.collide(player, moveTarget, function() {
-            player.body.velocity.set(0);
-            moveTarget.destroy();
-        });
+    game.physics.arcade.collide(player, layer, reachedDestination);
+    game.physics.arcade.collide(player, moveTarget, reachedDestination);
 };
 
 module.exports = state;
