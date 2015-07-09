1b15e0bed2f60f901f4b0e06de568bf67542e2d5
Michael Zekoff <zekoff@gmail.com>
2015-07-09 02:28:17 +0000

    Add multiple controllable players.
    Issues with event propagation remain.

--- src/state/main.js
+++ src/state/main.js
@@ -7,14 +7,20 @@ var WORLD_WIDTH = 64;
 var WORLD_HEIGHT = 64;
 
 var players;
+var currentPlayer;
 var layer;
 var moveTarget;
 
 state.create = function() {
-    players = [];
-    players.push(new Player(50,50));
-    game.add.existing(players[0]);
-    game.camera.follow(players[0]);
+    players = game.add.group();
+    var p1 = new Player(50,50);
+    var p2 = new Player(100,50);
+    var p3 = new Player(50,100);
+    players.add(p1);
+    players.add(p2);
+    players.add(p3);
+    currentPlayer = p1;
+    game.camera.follow(currentPlayer);
     game.camera.roundPx = false;
     var map = game.add.tilemap();
     map.addTilesetImage('test_tileset', 'test_tileset', 32, 32, 32, 32, 1);
@@ -37,10 +43,39 @@ state.create = function() {
         moveTarget.anchor.set(0.5);
         game.physics.enable(moveTarget);
     });
+
+    // add HUD
+    var hud = {};
+    var TEXT_STYLE = {
+        fontSize: 30,
+        fill: 'green'
+    };
+    var p0 = game.add.text(0,0, "P0000", TEXT_STYLE);
+    hud.p1 = game.add.text(0, 0, "P1", TEXT_STYLE);
+    hud.p2 = game.add.text(300, 0, "P2", TEXT_STYLE);
+    hud.p3 = game.add.text(600, 0, "P3", TEXT_STYLE);
+    var activatePlayer = function(player, sprite, pointer) {
+        console.log(pointer);
+        console.log('player activated');
+        currentPlayer = player;
+        game.camera.follow(currentPlayer);
+        console.log(sprite.input.consumePointerEvent);
+    };
+    [p0, hud.p1, hud.p2, hud.p3].forEach(function(e){
+        e.inputEnabled = true;
+        e.fixedToCamera = true;
+        // debugger;
+        e.input.useHandCursor = true;
+        // e.input.consumePointerEvent = true;
+    });
+    p0.events.onInputUp.add(function(){console.log('hit');});
+    hud.p1.events.onInputUp.add(activatePlayer.bind(null, p1));
+    hud.p2.events.onInputUp.add(activatePlayer.bind(null, p2));
+    hud.p3.events.onInputUp.add(activatePlayer.bind(null, p3));
 };
 
 var reachedDestination = function() {
-    players[0].body.velocity.set(0);
+    currentPlayer.body.velocity.set(0);
     if (moveTarget) {
         moveTarget.destroy();
         moveTarget = null;
@@ -48,9 +83,9 @@ var reachedDestination = function() {
 };
 
 state.update = function() {
-    if (moveTarget) game.physics.arcade.moveToXY(players[0], moveTarget.x, moveTarget.y, 300);
-    game.physics.arcade.collide(players[0], layer);
-    game.physics.arcade.collide(players[0], moveTarget, reachedDestination);
+    if (moveTarget) game.physics.arcade.moveToXY(currentPlayer, moveTarget.x, moveTarget.y, 300);
+    game.physics.arcade.collide(currentPlayer, layer);
+    game.physics.arcade.collide(currentPlayer, moveTarget, reachedDestination);
 };
 
 module.exports = state;
